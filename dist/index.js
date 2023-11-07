769bc6b9f18a907675f1a34c292c095add037f5c
marc_caixas <m.caixasmarin@gmail.com>
2023-11-07 10:19:11 +0100

    first commit

---
+++ dist/index.js
@@ -0,0 +1,46 @@
+"use strict";
+let p = document.getElementById("joke");
+let p2 = document.getElementById("meteo");
+const reportJokes = [];
+const options = {
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json'
+    }
+};
+const jokeApis = ["https://icanhazdadjoke.com/", "https://api.chucknorris.io/jokes/random"];
+let dadJoke = false;
+let i = 0;
+function getJoke() {
+    fetch(jokeApis[i], options)
+        .then(res => res.json())
+        .then(response => {
+        if (i == 0) {
+            p.textContent = '"' + response.joke + '"';
+            i = 1;
+        }
+        else {
+            p.textContent = '"' + response.value + '"';
+            i = 0;
+        }
+    });
+}
+;
+getJoke();
+function rateJoke(score) {
+    const date = new Date().toISOString();
+    const joke = {
+        joke: document.getElementById("joke").textContent,
+        score: score,
+        date: date
+    };
+    const found = reportJokes.find((element) => element.joke === joke.joke);
+    found == null ? reportJokes.push(joke) : found.score = score;
+    console.log(reportJokes);
+}
+fetch("https://www.el-tiempo.net/api/json/v2/home", options)
+    .then(res => res.json())
+    .then(response => {
+    p2.textContent = 'El tiempo hoy: ' + response.ciudades[0].stateSky.description;
+});
+//# sourceMappingURL=index.js.map
